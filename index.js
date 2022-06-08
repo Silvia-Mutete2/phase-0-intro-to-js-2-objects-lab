c1b59ea69eac80103f17872d1557be86e54abf4f
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-08 15:33:52 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-// Write your solution in this file!
 
-
-const employee = {
-    name: 'Sam'
+const employee = {name: 'Sam'
 }
-
+//updateEmployeeWithKeyAndValue(employee, key, value)
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     const newEmployee = {...employee}
-    newEmployee['streetAddress'] = '11 Broadway'
-
+    newEmployee.streetAddress= '11 Broadway'
     return newEmployee
 }
 
+//updates `employee` with the given `key` and `value
 function destructivelyUpdateEmployeeWithKeyAndValue(Employee, key, value) {
     const streetAddress = 'delete employee.name'
-    employee['streetAddress'] = '12 Broadway'
-
+    employee.streetAddress= '12 Broadway'
     return employee
 }
 
+//deletes `key` from a clone of employee and returns the new employee (it is non-destructive
 function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = {newEmployee}
     return newEmployee
 }
 
+//returns employee without the deleted key/value pair',
 function destructivelyDeleteFromEmployeeByKey(employee, Key) {
     delete employee.name
     return employee
